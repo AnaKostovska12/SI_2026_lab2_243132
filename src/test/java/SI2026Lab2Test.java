d6b4f4f3342c66730792aa2675b5bd336bb44c56
Ana Kostovska <ana.kostovska@students.finki.ukim.mk>
2026-05-08 22:28:38 +0200

    SI lab2 - CFG, tests, Gradle project

---
+++ src/test/java/SI2026Lab2Test.java
@@ -0,0 +1,76 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+public class SI2026Lab2Test {
+
+    @Test
+    public void searchBookEveryStatementTest() {
+        Library library = new Library();
+        library.addBook(new Book("Clean Code", "Robert C. Martin", "Programming"));
+        library.addBook(new Book("The Hobbit", "J.R.R. Tolkien", "Fantasy"));
+
+
+        assertThrows(IllegalArgumentException.class, () -> library.searchBookByTitle(""));
+
+        List<Book> result = library.searchBookByTitle("Clean Code");
+        assertNotNull(result);
+        assertEquals(1, result.size());
+
+        List<Book> result2 = library.searchBookByTitle("Harry Potter");
+        assertNull(result2);
+
+
+    }
+
+    @Test
+    public void borrowBookEveryBranchTest() {
+        Library library = new Library();
+        library.addBook(new Book("Clean Code", "Robert C. Martin", "Programming"));
+
+        assertThrows(IllegalArgumentException.class, () -> library.borrowBook("", ""));
+
+        assertThrows(RuntimeException.class, () -> library.borrowBook("Unknown", "Unknown"));
+
+        library.borrowBook("Clean Code", "Robert C. Martin");
+        assertTrue(library.countAvailableBooks() == 0);
+        assertThrows(RuntimeException.class, () -> library.borrowBook("Clean Code", "Robert C. Martin"));
+    }
+    @Test
+    public void searchBookMultipleConditionTest() {
+        Library library = new Library();
+        library.addBook(new Book("Clean Code", "Robert C. Martin", "Programming"));
+
+        assertNotNull(library.searchBookByTitle("Clean Code"));
+
+        assertNull(library.searchBookByTitle("Unknown"));
+
+        library.borrowBook("Clean Code", "Robert C. Martin");
+        assertNull(library.searchBookByTitle("Clean Code"));
+    }
+
+
+//        // T || F(MISLAM DEKA OVA E NEPOTREBNO)
+//        try { library.borrowBook("", "Author"); fail(); } catch (IllegalArgumentException e) { }
+
+
+    @Test
+    public void borrowBookMultipleConditionTest() {
+
+        Library library = new Library();
+        library.addBook(new Book(
+                "Clean Code",
+                "Robert C. Martin",
+                "Programming"));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> library.borrowBook("", ""));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> library.borrowBook("Title", ""));
+
+        library.borrowBook("Clean Code", "Robert C. Martin");
+
+        assertTrue(library.countAvailableBooks() == 0);
+    }
+}
